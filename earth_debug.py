77c6fe198e40a5e5eaeb0987bcf3edbf60b099ec
Repnikov Pavel <prepnik@mail.ru>
2023-12-17 23:10:53 +0300

    final

--- earth_debug.py
+++ earth_debug.py
@@ -3,69 +3,12 @@ from tqdm import tqdm
 import matplotlib
 import string
 import plotly.graph_objs as go
+from scipy import ndimage, datasets
+from matplotlib.widgets import Button, Slider
+
 
 hx = 1.0
 hy = 1.0
-tau = hx/10**4
-T = 2.0
-t0 = 2005.0
-
-@jit(nopython=True)
-def F(t,x,y,u):
-    return 0.0
-@jit(nopython =True)
-def D(x,y,u,kmask,xindex,yindex):
-    return kmask[xindex][yindex]
-
-@jit(nopython=True)
-def solve(Nx,Ny,Nt,hx,hy,tau,t,x,y,u_, water_mask_,earth_,kmask):
-    solutions = np.zeros(shape=(Nt,Nx,Ny))
-    solutions[0] = np.copy(u_)
-    u = np.copy(u_)
-    end_ = False
-    iters_ = 1
-    # N = Nx*Ny
-    # A = np.zeros(shape=(N,N))
-    for k in range(Nt-1):
-        print(k,Nt, np.sum(u)/10**9)
-        u_s = np.copy(u)
-        for s in range(iters_):
-            for i in range(1,Nx-1):
-                for j in range(1,Ny-1):
-                    # L_x = (D(x[i+1],y[j],u_s[i+1][j],kmask,i+1,j)-D(x[i-1],y[j],u_s[i-1][j],kmask,i-1,j))*(u[i+1][j]-u[i-1][j])/4.0/hx**2 + D(x[i],y[j],u_s[i][j],kmask,i,j)*(u[i+1][j]-2.0*u[i][j]+u[i-1][j])/hx**2
-                    # L_y = (D(x[i],y[j+1],u_s[i][j+1],kmask,i,j+1)-D(x[i],y[j-1],u_s[i][j-1],kmask,i,j-1))*(u[i][j+1]-u[i][j-1])/4.0/hy**2 + D(x[i],y[j],u_s[i][j],kmask,i,j)*(u[i][j+1]-2.0*u[i][j]+u[i][j-1])/hy**2
-                    L_x = 1.0/hx**2*(u[i+1][j]-u[i][j])*np.sqrt(D(x[i+1],y[j],u_s[i+1][j],kmask,i+1,j)*D(x[i],y[j],u_s[i][j],kmask,i,j)) - 1.0/hx**2*(u[i][j]-u[i-1][j])*np.sqrt(D(x[i],y[j],u_s[i][j],kmask,i,j)*D(x[i-1],y[j],u_s[i-1][j],kmask,i-1,j))
-                    L_y = 1.0/hy**2*(u[i][j+1]-u[i][j])*np.sqrt(D(x[i],y[j+1],u_s[i][j+1],kmask,i,j+1)*D(x[i],y[j],u_s[i][j],kmask,i,j)) - 1.0/hy**2*(u[i][j]-u[i][j-1])*np.sqrt(D(x[i],y[j],u_s[i][j],kmask,i,j)*D(x[i],y[j-1],u_s[i][j-1],kmask,i,j-1))
-                    u_s[i][j] = u[i][j] + tau*(L_x+ L_y + F(t[k],x[i],y[j],u_s[i][j]))
-                    # if u_s[i][j] < 0.0:
-                    #     print(1)
-                    #     fig,ax = plt.subplots()
-                    #     ax.scatter(y, u_s[i,:],label='N')
-                    #     ax.scatter(y, kmask[i,:],label=r'$\chi$')
-                    #     ax.scatter([y[j]],[0.0],label='bad point')
-
-                    #     ax.set_yscale('symlog')
-                    #     for p_ in range(len(y)):
-                    #         ax.axvline(y[p_],c='k',alpha=0.3)
-                    #     ax.legend()
-                    #     plt.show()
-
-        is_nan = np.sum(np.isnan(u_s)) > 0
-        is_inf = np.sum(np.isinf(u_s)) > 0
-        is_neg = np.sum(u_s < 0.0) > 0
-        if is_nan or is_inf or is_neg: 
-            end_ = True
-            if is_nan:
-                print('nan')
-            if is_inf:
-                print('inf')
-            if is_neg:
-                print('neg')
-        if end_:
-            return solutions[:k+1]
-        u = np.copy(u_s)
-        solutions[k+1] = np.copy(u_s) 
-    return solutions
 
 world_pop = gpd.read_file('./data/pop.geo.json')
 world_pop['POP2005']=world_pop['POP2005'].astype(float)
@@ -73,14 +16,14 @@ world_pop['area']=  world_pop.to_crs(6933).area.astype(float)*0.000001
 world_pop['density'] = (world_pop['POP2005'].div(world_pop['area']))
 
 country_name_index = {}
-for i in tqdm(range(len(world_pop))):
+for i in range(len(world_pop)):
     row_i = world_pop.iloc[i]
     country_name_index.update({row_i['NAME']:i})
 
 polygons_ = get_polygons(world_pop['geometry'])
 source_polygons = get_source_polygons(world_pop['geometry'])
 N_vec_ = world_pop['POP2005'].values
-for i in tqdm(range(len(N_vec_))):
+for i in range(len(N_vec_)):
     if pd.isna(N_vec_[i]):
         N_vec_[i]=1.0
 Names_ = world_pop['NAME'].values
@@ -88,18 +31,18 @@ Areas_ = world_pop['area'].values
 
 bounds_ = bounds_by_polygons(polygons_)
 xgrid,ygrid = make_grids(hx,hy,bounds_)
-
+Nx = len(xgrid)
+Ny = len(ygrid)
 
 masks_per_state = get_binary_mask_per_state_cv2(source_polygons,
                                 xgrid, ygrid)
 mask_of_earth = CUPmasks(masks_per_state)
 mask_of_water = np.logical_not(mask_of_earth)
 
-
 masks_with_N = [masks_per_state[i].astype(np.float64)*N_vec_[i] for i in range(len(masks_per_state))]
 
 mask_of_earth_N = None 
-for i in tqdm(range(len(masks_with_N))):
+for i in range(len(masks_with_N)):
     if mask_of_earth_N is None:
         mask_of_earth_N = masks_with_N[i]
     else:
@@ -107,9 +50,8 @@ for i in tqdm(range(len(masks_with_N))):
 
 cities = pd.read_csv('./data/worldcities.csv')
 
-
 cities_mask = np.zeros(shape=(len(xgrid),len(ygrid)),dtype=np.float64)
-for i in tqdm(range(len(cities))):
+for i in range(len(cities)):
     x = cities.iloc[i]['lng']
     y = cities.iloc[i]['lat']
     z = cities.iloc[i]['population']
@@ -119,55 +61,137 @@ for i in tqdm(range(len(cities))):
         z = 1.0
     cities_mask[xi][yi] +=  z
 
-
 poly_per_state = get_polygons_per_state(world_pop['geometry'])
-Nx = len(xgrid)
-Ny = len(ygrid)
-u_nu = np.zeros(shape=(Nx,Ny))
-for k in country_name_index:
-    country_name_ = k
-    c_mask = np.copy(masks_per_state[country_name_index[country_name_]])
-    c_N = N_vec_[country_name_index[country_name_]]
-    c_grid_count = np.sum(c_mask)
-    c_boundary_conditions_mask = np.logical_not(c_mask).astype(np.float64)
-    c_cities_vs = cities_mask*c_mask.astype(np.float64)
-    c_cities_vs_sum = np.sum(c_cities_vs)
-    number_of_cities_in_country_with_non_zero_people=  np.sum(c_cities_vs>0)
-    if number_of_cities_in_country_with_non_zero_people == 0:
-        continue
-    amnt_per_vertex = c_N/c_grid_count
-    c_cities_vs = c_cities_vs/np.sum(c_cities_vs)
-    c_cities_vs = c_cities_vs*c_N
-    u_nu += c_cities_vs
+
 
 borders_per_state = get_binary_mask_per_state(poly_per_state,xgrid,ygrid,hx,hy)
 all_borders_ = CUPmasks(borders_per_state)
+# all_borders_ = ndimage.median_filter(all_borders_, size=2)
 kmask = 1.0-np.logical_or(all_borders_.astype(bool), mask_of_water).astype(np.float64)
 
 
+u_nu = np.zeros(shape=(Nx,Ny))
+total_unaccounted = 0.0
+total_sum_ = 0.0
 
+for i in range(len(masks_per_state)):
+    state_mask = masks_per_state[i].astype(bool)
+    state_border_mask = borders_per_state[i].astype(bool)
+    internal_mask_ = np.logical_and(np.logical_not(state_border_mask), state_mask).astype(np.float64)
+    border_people_amt_ = np.sum(cities_mask*state_border_mask.astype(np.float64))
+    vertex_amt_internal_ = np.sum(internal_mask_)
+    if vertex_amt_internal_ ==0.0:
+        total_unaccounted += border_people_amt_
+        continue
+    total_sum_ += border_people_amt_
+    internal_people_ = internal_mask_*cities_mask + border_people_amt_/vertex_amt_internal_*internal_mask_
+    u_nu += internal_people_
+
+where_not_zero = u_nu !=0.0
+vertex_amnt_where_not_zero = np.sum(where_not_zero)
+u_nu[where_not_zero] += total_unaccounted/vertex_amnt_where_not_zero
+u_nu[where_not_zero] = u_nu[where_not_zero]/np.sum(u_nu)*np.sum(N_vec_)
+
+@jit(nopython=True)
+def F(uij, current_sum):
+    C0 = 279.0*10**9
+    return uij*current_sum/C0
+
+@jit(nopython =True)
+def D(x,y,u,kmask,xindex,yindex):
+    return kmask[xindex][yindex]
+
+@jit(nopython =True)
+def solve(Nx,Ny,Nt,hx,hy,tau,t,x,y,u_,years_,water_mask_,earth_,kmask):
+    solutions = np.zeros(shape=(len(years_),Nx,Ny))
+    solutions[0] = np.copy(u_)
+    u = np.copy(u_)
+    end_ = False
+    l_ = 1
+    u_next = np.zeros(shape=(Nx,Ny))
+    # d_m = 
+    # sources_ = []
+    # Lxvec =[]
+    for k in range(Nt-1):
+        # if np.mod(int(float(k+1)/float(Nt-2)*100),10) == 0:
+        current_sum = np.sum(u)
+        if np.isnan(current_sum) or np.isinf(current_sum):
+            return solutions[:l_]
+        print(k,Nt, current_sum/10**9)
+        for i in range(1,Nx-1):
+            for j in range(1,Ny-1):
+                # if u[i][j] < 0.0:
+                #     u__ = u[i][j]
+                #     print(1)
+                sq1x = D(x[i+1],y[j],u[i+1][j],kmask,i+1,j)*D(x[i],y[j],u[i][j],kmask,i,j)
+                sq2x = D(x[i],y[j],u[i][j],kmask,i,j)*D(x[i-1],y[j],u[i-1][j],kmask,i-1,j)
+                sq1y = D(x[i],y[j+1],u[i][j+1],kmask,i,j+1)*D(x[i],y[j],u[i][j],kmask,i,j)
+                sq2y = D(x[i],y[j],u[i][j],kmask,i,j)*D(x[i],y[j-1],u[i][j-1],kmask,i,j-1)
+                # if np.isnan(sq1x) or np.isnan(sq2x) or np.isnan(sq1y) or np.isnan(sq1y) or np.isinf(sq1x) or np.isinf(sq2x) or np.isinf(sq1y) or np.isinf(sq1y):
+                #     print(sq1x,sq2x)
+                #     print(sq1y,sq2y)
+
+                # if sq1x< 0.0 or  sq2x < 0.0 or sq1y<0.0 or  sq2y < 0.0:
+                #     print(sq1x,sq2x)
+                #     print(sq1y,sq2y)
+
+                L_x = 1.0/hx**2*(u[i+1][j]-u[i][j])*np.sqrt(sq1x) - 1.0/hx**2*(u[i][j]-u[i-1][j])*np.sqrt(sq2x)
+                L_y = 1.0/hy**2*(u[i][j+1]-u[i][j])*np.sqrt(sq1y) - 1.0/hy**2*(u[i][j]-u[i][j-1])*np.sqrt(sq2y)
+                # source_ = F(t[k],x[i],y[j],u,i,j)
+                source_ = F(u[i][j],current_sum)
+                # sources_.append(source_)
+                # Lxvec.append(L_x)
+                u_next[i][j] = np.maximum(u[i][j] + tau*(L_x+ L_y + source_),0.0)
+
+        # fig,ax = FloatDistr(sources_)
+        # ax.set_title('F')
+        # ax.set_xscale('log')
+        # fig,ax = FloatDistr(Lxvec)
+        # ax.set_title('D')
+        # ax.set_xscale('symlog')
+        # plt.show()
+        # break
+    
+        # is_nan = np.sum(np.isnan(u_next)) > 0
+        # is_inf = np.sum(np.isinf(u_next)) > 0
+        # is_neg = np.sum(u_next < 0.0) > 0
+        # if is_nan or is_inf or is_neg: 
+        #     end_ = True
+        #     if is_nan:
+        #         print('nan')
+        #     if is_inf:
+        #         print('inf')
+        #     if is_neg:
+        #         print('neg')
+        # if end_:
+        # return solutions[:l_]
+        u = np.copy(u_next)
+        for ye_ in years_:
+            if ye_ == t[k+1]:
+                solutions[l_] = np.copy(u_next)
+                l_ +=1  
+    return solutions
+
+tau = hx/10**3
+T = 40.0
+t0 = 2005.0
 Nt = int((t0+T-t0)/tau) + 1
+years_ = np.arange(t0,T+t0)
 tgrid = np.linspace(start=t0, stop=t0+T,num=Nt)
 t = np.copy(tgrid)
 x = np.copy(xgrid)
 y = np.copy(ygrid)
-u_vec = solve(Nx,Ny,Nt,hx,hy,tau,t,x,y,u_nu,mask_of_water.astype(np.float64),mask_of_earth.astype(np.float64),kmask)
+u_vec = solve(Nx,Ny,Nt,hx,hy,tau,t,x,y,u_nu,years_,mask_of_water.astype(np.float64),mask_of_earth.astype(np.float64),kmask)
 
-yaers_ = np.arange(t0,T+t0)
-solutions_ = []
-for i in range(len(u_vec)):
-    t_ = tgrid[i]
-    if np.isin(t_, yaers_):
-        solutions_.append(u_vec[i])
 
-from matplotlib.widgets import Button, Slider
-
+years_ = years_[:len(u_vec)]
 fig, ax = plt.subplots()
 ax.set_xticklabels([])
 ax.set_yticklabels([])
 fig.set_size_inches(16,9)
-cax = ax.matshow(solutions_[0].T,cmap=matplotlib.cm.jet,norm=matplotlib.colors.LogNorm())
-ax.set_title(f'{int(yaers_[0])} {np.round(np.sum(solutions_[0])/10**9,1)} млрд')
+# cax = ax.matshow(u_vec[0].T,cmap=matplotlib.cm.jet,norm=matplotlib.colors.LogNorm())
+cax = ax.matshow(u_vec[0].T,cmap=matplotlib.cm.jet,norm=matplotlib.colors.LogNorm())
+ax.set_title(f'{int(years_[0])} {np.round(np.sum(u_vec[0])/10**9,1)} млрд')
 ax.invert_yaxis()
 fig.subplots_adjust(left=0.0, bottom=0.0)
 # cbar = fig.colorbar(cax)
@@ -177,15 +201,15 @@ axfreq = fig.add_axes([0.1, 0.0, 0.5, 0.03])
 freq_slider = Slider(
     ax=axfreq,
     label='',
-    valmin=yaers_[0],
-    valmax=yaers_[-1],
-    valinit=yaers_[0]
+    valmin=years_[0],
+    valmax=years_[-1],
+    valinit=years_[0]
 )
 
 def update(val):
-    # ax.matshow(solutions_[0].T,cmap=matplotlib.cm.jet,norm=matplotlib.colors.LogNorm())
-    cax.set_data(solutions_[int(val- yaers_[0])].T)
-    ax.set_title(f'{int(val)} {np.round(np.sum(solutions_[int(val- yaers_[0])])/10**9,1)} млрд')
+    # ax.matshow(u_vec[int(val- years_[0])].T,cmap=matplotlib.cm.jet,norm=matplotlib.colors.LogNorm())
+    cax.set_data(u_vec[int(val- years_[0])].T)
+    ax.set_title(f'{int(val)} {np.round(np.sum(u_vec[int(val- years_[0])])/10**9,1)} млрд')
     fig.canvas.draw_idle()
     # plt.draw()
 
